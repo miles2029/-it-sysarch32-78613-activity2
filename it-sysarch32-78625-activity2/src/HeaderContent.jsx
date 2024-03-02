77eac108ede4b851517157ebf6bd67047832e230
Miles Maratas <78625-22217897@SERVER2.DOMAIN>
2024-03-02 15:53:06 +0800

    footer and header

---
+++ it-sysarch32-78625-activity2/src/HeaderContent.jsx
@@ -0,0 +1,17 @@
+function Header() {
+    return(
+        <header>
+            
+            <h1>Miles</h1>
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Services</li>
+                    <li>Contact</li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
+export default Header
