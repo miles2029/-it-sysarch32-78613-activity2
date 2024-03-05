e39e51d47d60f3aaa9d61c7e94dbc486cc111ffd
Miles D. Maratas <160054710+miles2029@users.noreply.github.com>
2024-03-05 22:09:56 +0800

    Delete it-sysarch32-78625-activity2 directory

--- it-sysarch32-78625-activity2/src/footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-    return(
-        <footer>
-            
-         <p>&copy; 2024  IT-SYSARCH32-78625 Made by Miles Maratas</p>
-        </footer>
-    );
-}
-export default Footer
